0069cd2e73e1600bfda68511ea7acf9b27edec8f
lucadard <lucadardenne@hotmail.com>
2023-05-16 13:13:36 -0300

    se agregan comentarios

--- TP/Solucion.hs
+++ TP/Solucion.hs
@@ -1,7 +1,9 @@
+-- PARA HACER:
+-- MEJORAR COMENTARIOS
 module Solucion where
 
 -- Completar con los datos del grupo
---
+
 -- Nombre de Grupo: facts
 -- Integrante 1: Teo Nabot, teonabot@gmail.com, 996/22
 -- Integrante 2: Ariel Bakal, bakalariel2002@gmail.com, 1014/22
@@ -41,6 +43,7 @@ likesDePublicacion (_, _, us) = us
 
 -- Ejercicios
 
+{- EJERCICIO 1 -}
 -- Devuelve una lista de Usuarios sin repetir de una Red Social
 nombresDeUsuarios :: RedSocial -> [String]
 nombresDeUsuarios (us, rels, pubs) = limpiarRepetidos (nombresDeUsuariosConRepetidos us)
@@ -49,8 +52,8 @@ pertenece :: Eq t => t -> [t] -> Bool
 pertenece _ [] = False
 pertenece a (x : xs) = a == x || pertenece a xs
 
--- La función limpiarRepetidos recibe una lista de cadenas de caracteres y devuelve la lista sin las cadenas de caracteres repetidas.
-limpiarRepetidos :: [String] -> [String]
+-- Devuelve la lista sin los elementos repetidos.
+limpiarRepetidos :: Eq a => [a] -> [a]
 limpiarRepetidos [] = []
 limpiarRepetidos (x : xs)
   | pertenece x xs = limpiarRepetidos xs
@@ -60,6 +63,9 @@ nombresDeUsuariosConRepetidos :: [Usuario] -> [String]
 nombresDeUsuariosConRepetidos [] = []
 nombresDeUsuariosConRepetidos us = nombreDeUsuario (head us) : nombresDeUsuariosConRepetidos (tail us)
 
+{- EJERCICIO 1 -}
+
+{- EJERCICIO 2 -}
 amigosDe :: RedSocial -> Usuario -> [Usuario]
 amigosDe (_, rels, _) = relacionadosAUsuario rels
 
@@ -71,15 +77,21 @@ relacionadosAUsuario (x : xs) u
   | u == snd x = fst x : relacionadosAUsuario xs u
   | otherwise = relacionadosAUsuario xs u
 
+{- EJERCICIO 2 -}
+
+{- EJERCICIO 3 -}
 -- dada una red social y un usuario devuelve la cantidad de amigos del usuario, viendo la cantidad de elemntos que tiene la lista de amigos
 cantidadDeAmigos :: RedSocial -> Usuario -> Int
 cantidadDeAmigos rs u = cantidadDeElementos (amigosDe rs u)
 
--- dada una lista de usuarios, devuelve la cantidad de usuarios que tiene esa lista
+-- dada una lista devuelve la cantidad items de la lista
 cantidadDeElementos :: [a] -> Int
 cantidadDeElementos [] = 0
 cantidadDeElementos (x : xs) = 1 + cantidadDeElementos xs
 
+{- EJERCICIO 3 -}
+
+{- EJERCICIO 4 -}
 -- describir qué hace la función: .....
 usuarioConMasAmigos :: RedSocial -> Usuario
 usuarioConMasAmigos (u : us, rels, pubs) = usuarioConMasAmigosAux (u : us, rels, pubs) us u
@@ -90,6 +102,9 @@ usuarioConMasAmigosAux red (u : us) l
   | cantidadDeAmigos red l >= cantidadDeAmigos red u = usuarioConMasAmigosAux red us l
   | otherwise = usuarioConMasAmigosAux red us u
 
+{- EJERCICIO 4 -}
+
+{- EJERCICIO 5 -}
 -- Determina si hay un usuario con mas de 1000000 de amigos
 estaRobertoCarlos :: RedSocial -> Bool
 estaRobertoCarlos ([], _, _) = False
@@ -97,6 +112,9 @@ estaRobertoCarlos (us, rels, pubs)
   | cantidadDeAmigos (us, rels, pubs) (head us) > 1000000 = True
   | otherwise = estaRobertoCarlos (tail us, rels, pubs)
 
+{- EJERCICIO 5 -}
+
+{- EJERCICIO 6 -}
 -- Devuelve las publicaciones de un usuario
 publicacionesDe :: RedSocial -> Usuario -> [Publicacion]
 publicacionesDe (us, rels, []) u = []
@@ -104,6 +122,9 @@ publicacionesDe (us, rels, p : pubs) u
   | u == usuarioDePublicacion p = p : publicacionesDe (us, rels, pubs) u
   | otherwise = publicacionesDe (us, rels, pubs) u
 
+{- EJERCICIO 6 -}
+
+{- EJERCICIO 7 -}
 -- describir qué hace la función: .....
 publicacionesQueLeGustanA :: RedSocial -> Usuario -> [Publicacion]
 publicacionesQueLeGustanA (us, rels, []) u = []
@@ -111,6 +132,9 @@ publicacionesQueLeGustanA (us, rels, p : pubs) u
   | pertenece u (likesDePublicacion p) = p : publicacionesQueLeGustanA (us, rels, pubs) u
   | otherwise = publicacionesQueLeGustanA (us, rels, pubs) u
 
+{- EJERCICIO 7 -}
+
+{- EJERCICIO 8 -}
 -- describir qué hace la función: .....
 lesGustanLasMismasPublicaciones :: RedSocial -> Usuario -> Usuario -> Bool
 lesGustanLasMismasPublicaciones (us, rels, []) u1 u2 = True
@@ -118,6 +142,9 @@ lesGustanLasMismasPublicaciones (us, rels, p : pubs) u1 u2 =
   (pertenece u1 (likesDePublicacion p) == pertenece u2 (likesDePublicacion p))
     && lesGustanLasMismasPublicaciones (us, rels, pubs) u1 u2
 
+{- EJERCICIO 8 -}
+
+{- EJERCICIO 9 -}
 -- un usuario debe haberle dado like a todas las publicaciones del usuario parametro
 tieneUnSeguidorFiel :: RedSocial -> Usuario -> Bool
 tieneUnSeguidorFiel ([], rels, pubs) u = False
@@ -127,13 +154,16 @@ tieneUnSeguidorFiel (us, rels, pubs) u
       incluido (publicacionesDe (us, rels, pubs) u) (publicacionesQueLeGustanA (us, rels, pubs) (head us))
         || tieneUnSeguidorFiel (tail us, rels, pubs) u
 
--- si una lista esta incluida en otra lista
+-- dice si una lista esta incluida en otra lista
 incluido :: Eq a => [a] -> [a] -> Bool
 incluido [] l = True
 incluido l1 l2
   | pertenece (head l1) l2 = incluido (tail l1) l2
   | otherwise = False
 
+{- EJERCICIO 9 -}
+
+{- EJERCICIO 10 -}
 -- "recorre" la lista de relaciones partiendo del usuario inicial,
 -- descartando en las que no se encuentra el usuario final
 existeSecuenciaDeAmigos :: RedSocial -> Usuario -> Usuario -> Bool
@@ -149,7 +179,6 @@ existeSecuenciaDeAmigos (users, rels, pubs) usuarioInicial usuarioFinal
     siguienteUsuario = relacionSinUsuario primeraRelacion usuarioInicial
     listaSinRelacion = quitarDeLista primeraRelacion rels
 
-{- aux -}
 -- devuelve el otro usuario en la relacion
 relacionSinUsuario :: Relacion -> Usuario -> Usuario
 relacionSinUsuario rel u
@@ -172,4 +201,4 @@ quitarDeLista item (x : xs)
   | item == x = quitarDeLista item xs
   | otherwise = x : quitarDeLista item xs
 
-{- aux -}
+{- EJERCICIO 10 -}
