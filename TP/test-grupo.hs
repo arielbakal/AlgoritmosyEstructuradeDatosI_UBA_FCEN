8c63716a806f9f72853a2706600db6d904371b30
Juan Cruz Berton <juanceberton@gmail.com>
2023-05-18 21:28:10 -0300

    Test ej9 completo_correcion test ej1 casoB_ No corri el codigo

--- TP/test-grupo.hs
+++ TP/test-grupo.hs
@@ -6,7 +6,7 @@ main = runTestTT casos -- para correr los tests ejecutar este archivo y escribir
 {- ESCRIBIR CASOS DE TEST ACÁ -}
 ejercicio1casoA = nombresDeUsuarios redA ~?= ["Teo", "Ariel", "Juan Cruz", "Luca", "Roberto Carlos"]
 
-ejercicio1casoB = nombresDeUsuarios redB ~?= ["Teo", "Ariel", "Luca"]
+ejercicio1casoB = nombresDeUsuarios redB ~?= ["Teo", "Ariel", "Juan Cruz", "Luca"]
 
 ejercicio1casoC = nombresDeUsuarios ([], [], []) ~?= []
 
@@ -32,7 +32,7 @@ ejercicio4casoB = usuarioConMasAmigos redA ~?= usuario4
 ejercicio4casoC = usuarioConMasAmigos ([usuario1, usuario2], [(usuario1, usuario2)], []) ~?= usuario1
 
 --
-ejercicio5casoA = estaRobertoCarlos red5 ~?= False
+ejercicio5casoA = estaRobertoCarlos ([],[],[]) ~?= False
 
 ejercicio5casoB = estaRobertoCarlos redA ~?= False
 
@@ -57,6 +57,14 @@ ejercicio8casoC = lesGustanLasMismasPublicaciones ([usuario1, usuario2], [], [(u
 
 ejercicio8casoD = lesGustanLasMismasPublicaciones redB usuario2 usuario5 ~?= False
 
+--
+ejercicio9casoA = tieneUnSeguidorFiel redB usuario5 ~?= False
+
+ejercicio9casoB = tieneUnSeguidorFiel redB usuario1 ~?= False
+
+ejercicio9casoC = tieneUnSeguidorFiel redC usuario5 ~?= True
+
+
 {- Y AGREGARLOS ACÁ con un nombre descriptivo -}
 casos =
   TestList
@@ -76,7 +84,7 @@ casos =
       TestLabel "usuarioConMasAmigos usuario unico" ejercicio4casoA,
       TestLabel "usuarioConMasAmigos usuario con mas amigos" ejercicio4casoB,
       TestLabel "usuarioConMasAmigos usuarios con misma cantidad de amigos" ejercicio4casoC,
-      --CASOS EJERCICIO 5
+      -- CASOS EJERCICIO 5
       TestLabel "estaRobertoCarlos lista de usuarios vacia" ejercicio5casoA,
       TestLabel "estaRobertoCarlos usuario con cantidadDeAmigos <10" ejercicio5casoB,
       TestLabel "estaRobertoCarlos usuario con cantidadDeAmigos >10" ejercicio5casoC,
@@ -92,6 +100,11 @@ casos =
       TestLabel "lesGustanLasMismasPublicaciones, likean las mismas publicaciones" ejercicio8casoC,
       TestLabel "lesGustanLasMismasPublicaciones, likean una misma publicacion pero las demas no" ejercicio8casoD
       
+      -- CASOS EJERCICIO 9
+      TestLabel "tieneUnSeguidorFiel usuario sin publicaciones" ejercicio9casoA,
+      TestLabel "tieneUnSeguidorFiel usuario con publicaciones sin seguidor fiel " ejercicio9casoB,
+      TestLabel "tieneUnSeguidorFiel usuario con publicaciones con seguidor fiel " ejercicio9casoC,
+
     ]
 
 {- DATOS -}
@@ -176,6 +189,16 @@ publicacion4_2 = (usuario4, "I am Bob", [])
 
 publicacion4_3 = (usuario4, "Just kidding, i am Mariela", [usuario1, usuario3])
 
+publicacion5_1 = (usuario5,"Alea iacta est", [usuario1, usuario3])
+ 
+publicacion5_2 = (usuario5,"Ab imo pectore", [usuario2, usuario3])
+ 
+publicacion5_3 = (usuario5,"Carpe diem", [usuario4, usuario3])
+ 
+publicacion5_4 = (usuario5,"Mens sana in corpore sano", [usuario3])
+ 
+publicacion5_5 = (usuario5,"Errare humanum est", [usuario3])
+
 -- red A
 usuariosA = [usuario1, usuario2, usuario3, usuario4, usuario6]
 
@@ -186,7 +209,7 @@ publicacionesA = [publicacion1_1, publicacion1_2, publicacion2_1, publicacion2_2
 redA = (usuariosA, relacionesA, publicacionesA)
 
 -- red B
-usuariosB = [usuario1, usuario2, usuario4, usuario5]
+usuariosB = [usuario1, usuario2, usuario3, usuario4, usuario5]
 
 relacionesB = [relacion1_2, relacion2_3]
 
@@ -201,12 +224,9 @@ usuariosC = [usuario1, usuario2, usuario3, usuario4, usuario5, usuario6, usuario
 
 relacionesC = [relacion1_2, relacion1_3, relacion1_4, relacion1_5, relacion1_6, relacion1_7, relacion1_8, relacion1_9, relacion1_10, relacion1_11, relacion1_12]
 
-publicacionesC = [publicacion1_3, publicacion1_4, publicacion1_5]
+publicacionesC = [publicacion1_3, publicacion1_4, publicacion1_5,publicacion5_1, publicacion5_2, publicacion5_3, publicacion5_4, publicacion5_5,]
 
 redC = (usuariosC, relacionesC, publicacionesC)
-
--- red 5 solo para caso A del ej 5
-red5= (usuariosC,[],publicacionesC)
 
 -- red d
 usuariosD = [usuario1, usuario2, usuario3, usuario4, usuario5, usuario6, usuario7, usuario8, usuario9, usuario10, usuario11, usuario12]
@@ -217,4 +237,3 @@ publicacionesD = [publicacion1_3, publicacion1_4, publicacion1_5]
 
 redD = (usuariosD, relacionesD, publicacionesD)
 
-s = []
