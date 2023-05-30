af9f8ba3a398ad2a004cb38eb1bca34543170453
Ariel Bakal <arielbakal.dev@gmail.com>
2023-05-30 20:45:36 -0300

    CMS2 - Archivos del Ejercicio 1

---
+++ CMS/quienGana.py
@@ -0,0 +1,10 @@
+import sys
+
+def quienGana(j1: str, j2: str) -> str : 
+    #Implementar esta funcion
+    return ""
+
+if __name__ == '__main__':
+  x = input()
+  jug = str.split(x)
+  print(quienGana(jug[0], jug[1]))
