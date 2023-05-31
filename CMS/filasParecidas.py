d5969b39a1680e11df13ea5cac78466b20f90e2a
Ariel Bakal <arielbakal.dev@gmail.com>
2023-05-31 14:48:07 -0300

    CMS2 - Ejercicio 4, 50%

--- CMS/filasParecidas.py
+++ CMS/filasParecidas.py
@@ -4,8 +4,33 @@ from typing import List
 # l: List[int]  <--Este es un ejemplo para una lista de enteros.
 # Respetar esta sintaxis, ya que el CMS dirá que no pasó ningún test si usan otra notación.
 def filasParecidas(matriz: List[List[int]]) -> bool :
-  # Implementar esta funcion
-  return True
+  res: bool = False
+  fila: List[int] = []
+  fila2: List[int] = []
+
+  if len(matriz) == 1:
+    return res
+  
+  elif len(matriz) > 1:
+
+    for s in range(0, len(matriz)): # Recorro filas
+
+        for r in range(0, len(matriz[0])): # Recorro la fila s y la guardo
+            fila.append(matriz[s][r])
+
+        for i in range(0, len(matriz)): # Recorro las filas != s
+            if i != s:
+                for j in range(0, len(matriz[0])):
+                    fila2.append(matriz[i][j])
+            if fila == fila2: # Comparo fila s con fila != s
+                res = True
+                return res
+            else:
+                fila2.clear()
+        
+        fila.clear()
+        
+  return res
 
 
 if __name__ == '__main__':
