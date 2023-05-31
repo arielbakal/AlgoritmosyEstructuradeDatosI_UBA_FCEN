dcd3d3fe604b558ee614d626945ca11d26911611
Ariel Bakal <arielbakal.dev@gmail.com>
2023-05-30 21:22:54 -0300

    CMS2 - Ejercicio 2, 100%

--- CMS/fibonacciNoRecursivo.py
+++ CMS/fibonacciNoRecursivo.py
@@ -1,8 +1,19 @@
 import sys
 
 def fibonacciNoRecursivo(n: int) -> int:
-  # Implementar esta funcion
-  return 0
+  res: int = 0
+  l: list = [0]
+
+  if n >= 1:
+    l.append(1)
+
+  if n > 1:
+    for i in range(2, n+1, 1):
+      l.append(l[i-1] + l[i-2])
+  
+  res = l[len(l)-1]
+
+  return res
 
 
 if __name__ == '__main__':
