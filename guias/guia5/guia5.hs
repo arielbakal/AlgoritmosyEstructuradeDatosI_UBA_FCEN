af46a47bf0cd9fc58eeb08423e3e116a1928914f
Ariel Bakal <arielbakal.dev@gmail.com>
2023-06-14 18:15:30 -0300

    P5 - Ejercicio 2

--- guias/guia5/guia5.hs
+++ guias/guia5/guia5.hs
@@ -2,6 +2,9 @@
 {-# HLINT ignore "Redundant bracket" #-}
 {-# HLINT ignore "Use foldr" #-}
 {-# OPTIONS_GHC -Wno-incomplete-patterns #-}
+import Distribution.InstalledPackageInfo (InstalledPackageInfo(includeDirs))
+import Distribution.Types.IncludeRenaming (IncludeRenaming(IncludeRenaming))
+import Text.XHtml (rev)
 
 -- Ejercicio 1
 
@@ -23,4 +26,71 @@ principio x = x
 reverso :: [t] -> [t]
 reverso [] = []
 reverso (x:xs) = reverso xs ++ [x]
+
+-- Ejercicio 2
+
+-- 2.1
+
+pertenece :: (Eq t) => t -> [t] -> Bool
+pertenece x xs = elem x xs
+
+pertenece2 :: (Eq t) => t -> [t] -> Bool
+pertenece2 e [] = False
+pertenece2 e (x:xs) | e == x = True 
+                    | otherwise = False || pertenece2 e xs 
+
+-- 2.2
+
+todosIguales :: (Eq t) => [t] -> Bool
+todosIguales [] = True
+todosIguales [x] = True
+todosIguales (x:xs) | x == head xs = todosIguales xs
+                    | otherwise = False
+
+-- 2.3
+
+todosDistintos :: (Eq t) => [t] -> Bool
+todosDistintos [] = True
+todosDistintos [x] = True
+todosDistintos (x:xs) = not ( elem x xs ) && todosDistintos xs
+
+-- 2.4
+
+hayRepetidos :: (Eq t) => [t] -> Bool
+hayRepetidos [] = False
+hayRepetidos [x] = False
+hayRepetidos (x:xs) = elem x xs || hayRepetidos xs
+
+-- 2.5
+
+quitar :: (Eq t) => t -> [t] -> [t]
+quitar e [] = []
+quitar e (x:xs) | e == x = xs
+                | otherwise = [x] ++ quitar e xs
+
+-- 2.6
+quitarTodos :: (Eq t) => t -> [t] -> [t]
+quitarTodos e [] = []
+quitarTodos e (x:xs) | e == x = quitarTodos e xs
+                     | otherwise = [x] ++ quitarTodos e xs  
+
+-- 2.7
+eliminarRepetidos :: (Eq t) => [t] -> [t]
+eliminarRepetidos [] = []
+eliminarRepetidos [x] = [x]
+eliminarRepetidos (x:xs) | elem x xs = eliminarRepetidos xs
+                         | otherwise = [x] ++ eliminarRepetidos xs  
+
+-- 2.8
+incluido [] [] = True
+incluido _ [] = False
+incluido [] y = True
+incluido (x:xs) y = elem x y && incluido xs y
+
+mismosElementos :: (Eq t) => [t] -> [t] -> Bool
+mismosElementos x y = incluido x y && incluido y x
+
+-- 2.9
+capicua :: (Eq t) => [t] -> Bool
+capicua xs = reverso xs == xs 
 
